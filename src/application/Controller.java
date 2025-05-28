b9e20844c2f1806587376ffa87761f05aaa0b17f
潘亮銓 <liangquan0527@gmail.com>
2025-05-28 11:10:45 +0800

    change start scene

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -8,13 +8,34 @@ import pet.PetWindow;
 
 public class Controller {
 
-    @FXML
+	@FXML
     private Button startButton;
+    @FXML
+    private ListView<checkBox> selectDesktopPet;
 
+    @FXML
     public void startButtonClicked(ActionEvent e) {
+    	// build the pet scene
     	Stage startStage = (Stage) startButton.getScene().getWindow();
     	startStage.close();
     	PetWindow petWindow = new PetWindow();
     	petWindow.show();
+    }
+    
+    @FXML
+    public void initialize() {
+    	// build up the checkBoxList
+    	ObservableList<checkBox> tmpList = FXCollections.observableArrayList();
+    	
+    	File mainDir = new File("../image");
+    	File[] subDirs = mainDir.listFiles(File::isDirectory);
+    	if (subDirs == null) {
+    		System.out.println("該路徑下並不存在資料夾");
+    		return;
+    	}
+    	for (File folder : subDirs) {
+    		tmpList.add(new checkBox(foler.getName()))
+    	}
+    	selectDesktopPet.setItems(tmpList);
     }
 }
